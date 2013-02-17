0e4d571195678b063bf9bbd4a3294aa1e8f56d09
bitpit <ejr10@hampshire.edu>
2013-02-17 12:34:29 -0500

    pushy

--- webgrep.rb
+++ webgrep.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'nokogiri'
 
 class Webgrep
-    
+    attr_accessor :doc, :base_url
     
     def initialize(reg_target, url, depth)
         @target = reg_target
@@ -13,6 +13,8 @@ class Webgrep
         @depth = depth
         @doc = Nokogiri::HTML(open(url))
         @depth_track = 0
+        @contains_query = []
+        
     end
     
     
@@ -33,20 +35,50 @@ class Webgrep
         
         raw = @doc.css("a")
         processed = []
-        raw.each {|x| processed << x.values[0]}
+        raw.each {|x| 
+            if x.values[0].length > 3 && x.values[0][0,3] != "jav" &&
+                (/(\.edu|\.com|\.info|\.org|\.co.uk|\.ru|\.eu|\.net)/).match(x.values[0]) != nil &&
+                (/mailto/).match(x.values[0]) == nil
+                processed << x.values[0]
+            end}
+        
         
         processed -= base_terminal
+        processed = processed.uniq()
         processed.each {|i|
             if i[0,3] != "htt"
                 i.insert(0,base_decoded)
             end
         }
-        return processed-@visited
-    end   
+        processed -= @visited
+        if (processed.uniq != nil)
+            return processed.uniq
+        else return processed
+        end
+    end 
     
+    
+    def search
+        text = @doc.xpath("//text()")
+        x = File.open("file.txt",'w')
+        text.each {|z| x.puts z}
+        x.close
+    end
+    
+    
+    def tester
+        raw = @doc.css("a")
+        processed = []
+        raw.each {|x| 
+            if x.values[0].length > 3 && x.values[0][0,3] != "jav"
+                processed << x.values[0]
+            end}
+        return processed
+    end
 
 end
 
 
 #g = Webgrep.new
 # args = ("(H|h)elmuth","http://people.cs.umass.edu/~thelmuth/index.html",3)
+#@doc.xpath("//text()") #all text
